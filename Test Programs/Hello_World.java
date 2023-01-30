d73ab7d236565a8342a6bb76b7a70a1c41fb35df
NattyEddy <heyitzn8@gmail.com>
2023-01-29 19:46:13 -0700

    Added comments and a header.

---
+++ Test Programs/Hello_World.java
@@ -0,0 +1,21 @@
+/*
+ *  Program: Hello_World.java
+ * 
+ *  Purpose: Obligatory test program to print "Hello World!"
+ * 
+ *  Date last modified: January 30, 2023
+ * 
+ *  Name: N******** E*******
+ * 
+ *  School: ******* ******* **** ******
+ * 
+ */
+
+public class Hello_World {
+    
+    // Prints "Hello World!"
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
